35153327fe694484719d4d570f77996338f6f4c3
GorkemEcer <gorkemecer06@gmail.com>
2023-07-07 20:59:59 +0300

    feat(library): Library implemented
    Kitap sayfa değiştirme eklendi.

--- NextPatientGame/Assets/Scripts/GameController/GameController.cs
+++ NextPatientGame/Assets/Scripts/GameController/GameController.cs
@@ -10,8 +10,8 @@ public class GameController : MonoBehaviour
 
     private void Awake()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
     private void Update()
     {
